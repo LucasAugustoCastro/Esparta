c7a75ca1f99e0433de29ed70d848d5a26fb77bfc
Lusca05 <40044388+Lusca05@users.noreply.github.com>
2020-01-23 14:44:36 -0300

    comentarios inseridos

--- Teste 3/js/js.js
+++ Teste 3/js/js.js
@@ -1,7 +1,7 @@
 var count = 1; 
 
 $(document).ready(function() {
-    $(document).keypress(function(e) {
+    $(document).keypress(function(e) {//clicar no botao de inserir pressionando enter
         if (e.which == 13){
             console.log($("#inputAdd").val());
             if($("#inputAdd").val() != ""){
@@ -10,17 +10,17 @@ $(document).ready(function() {
         }
     });
 
-    $('#button').click(function() {
-        var textAdd = $('#inputAdd').val();
-        $('#inputAdd').val('');
-
+    $('#button').click(function() {//botao de insersao de valores na div
+        var textAdd = $('#inputAdd').val();//Pega o valor no input e salva em uma variavel
+        $('#inputAdd').val(''); //esvasia o input
+        //Adiciona uma nova tarefa na div de tarefas
         $('#div').append('<div id = "divinha' + count + '"><input id = "check" type="checkbox"> ' + textAdd + '<img id = "lixo" src="imagens/lixeira.png" alt="" height="20px">' + '</div>');
         count++; 
     });
-    $(document).on("change", "#check", function() {
+    $(document).on("change", "#check", function() {// ao marcar o checkbox sera adionado a classe marcar na div
         $(this).parent().toggleClass('marcar');
     });
-    $(document).on("click", "#incompleto",function(){
+    $(document).on("click", "#incompleto",function(){//ao clicar no botao 'Em progresso' mostrara todas as tarefas que nao estiverem com o checkbox marcado
         $("#div").find('*').each(function(){
             var classe = $(this).attr("class");
             var id = $(this).attr("id"); 
@@ -38,7 +38,7 @@ $(document).ready(function() {
         });
 
     });
-    $(document).on("click", "#completo",function(){
+    $(document).on("click", "#completo",function(){//ao clicar no botao 'Finalizadas' mostrara todas as tarefas que estiverem com o checkbox marcado
         $("#div").find('*').each(function(){
             var classe = $(this).attr("class");
             var id = $(this).attr("id"); 
@@ -55,7 +55,7 @@ $(document).ready(function() {
             }
         });
     });
-    $(document).on("click", "#todos",function(){
+    $(document).on("click", "#todos",function(){//mostrara toda as tarefas novamente
         $("#div").find('*').each(function(){
             var classe = $(this).attr("class");
             var id = $(this).attr("id"); 
@@ -64,19 +64,8 @@ $(document).ready(function() {
             
         });
     });
-    /*if ($("#check").is(":checked")==false)
-        {
-            console.log($("#check").parent().class());
-            if($("#check").parent().getClass()=="marcar"){
-                $("#check").parent().hide();
-            }
-            
-            
-        }*/
-        
-  
 
-  $(document).on("click", '#lixo', function() {
+  $(document).on("click", '#lixo', function() {//Ao clicar na imagem de lixo, mostrar um span perguntando se realmente deseja excluir aquela tarefa, caso queira sera excluido
     if (confirm("Voce realmente deseja excluir esse item?") == true) {
       $(this).parent().remove();
     }
